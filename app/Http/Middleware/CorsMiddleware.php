1a5e34500f94514a4cb954ca80df14a9d99f0764
mao <mauricio.1020@hotmail.com>
2025-02-08 19:35:31 -0500

    add middleware cors

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Agrega los encabezados CORS necesarios
+        $response = $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin', 'https://decameron-frontend.netlify.app');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        return $response;
+    }
+}
